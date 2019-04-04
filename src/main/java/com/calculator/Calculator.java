db838f4b9e00222199d3820bbf1acd1185b01880
maciejgrzelec86 <maciej.kodilla.grzelec@gmail.com>
2019-04-04 14:33:21 +0200

    test task

---
+++ src/main/java/com/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.calculator;
+
+public class Calculator {
+
+    int variableA, variableB;
+
+    public int addition(int variableA, int variableB) {
+        return variableA + variableB;
+    }
+
+    public int subtraction(int variableA, int variableB) {
+        return variableB - variableA;
+    }
+}
---
+++ src/main/java/com/calculator/KodillaCourseApplication.java
@@ -0,0 +1,18 @@
+package com.calculator;
+
+import com.fasterxml.jackson.databind.BeanProperty;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourseApplication.class, args);
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addition(5, 6));
+    }
+
+
+
+}
